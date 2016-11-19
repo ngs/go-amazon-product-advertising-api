31ec7d7a3e964d823a3d26b6a0c5415f7ce6d4e2
Atsushi Nagase <a@ngs.io>
2016-11-19 12:54:41 +0900

    Remove generator

--- gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for OP in ItemSearch BrowseNodeLookup ItemLookup SimilarityLookup CartAdd CartClear CartCreate CartGet CartModify; do
-  FN=$(ruby -ractive_support -e "print ActiveSupport::Inflector.underscore('$OP')")
-  erb operation=$OP templates/main.erb | gofmt > amazon/$FN.go
-  erb operation=$OP templates/test.erb | gofmt > amazon/${FN}_test.go
-done
